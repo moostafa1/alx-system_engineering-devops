53f7bb1aa13f2764933025556f25ed509eb241a8
Mostafa <desham8@gmail.com>
2024-04-12 17:43:39 +0200

    editingg the puppet to install pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # Installing flask
-package { 'flask':
+package { 'pip3':
+  ensure   => 'installed',
+  provider => 'pip3',
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['pip3'],
 }
