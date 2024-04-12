aa40043df511e03c5446b216a1f25983f921eba4
Mostafa <desham8@gmail.com>
2024-04-12 17:53:46 +0200

    fixing commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,10 @@
-# Installing flask
-package { 'pip3':
-  ensure   => 'installed',
-  provider => 'pip3',
+# install flask
+package { 'python3-pip':
+  ensure => installed,
 }
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['pip3'],
+exec { 'install_Flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  require => Package['python3-pip'],
 }
