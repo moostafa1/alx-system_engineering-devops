31780828734dfbf60e26d02a05a4a93c094540ab
Mostafa <desham8@gmail.com>
2024-04-17 02:13:09 +0200

    fixing logic and adding main files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,12 @@
-#!/usr/bin/python3
-"""
-    Function to query the Reddit API and returns the number of subscribers
-"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Custom User"}
-    res = requests.get(url, headers=headers)
-    data = res.json().get("data").get("subscribers")
-    return data if data else 0
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
